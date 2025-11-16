f64014014d8bc182b768de54b6cec8b5de5e16fb
lospueblosmasbonitosdeespana <lospueblosmasbonitosdeespana@gmail.com>
2025-11-16 20:51:19 +0100

    Fix: GoogleSignInModule.swift corregido (import + signIn moderno)

--- ios/GoogleSignInModule.swift
+++ ios/GoogleSignInModule.swift
@@ -1,6 +1,7 @@
 import Foundation
 import GoogleSignIn
 import React
+import UIKit
 
 @objc(GoogleSignInModule)
 class GoogleSignInModule: NSObject {
@@ -9,15 +10,47 @@ class GoogleSignInModule: NSObject {
   func signIn(_ resolve: @escaping RCTPromiseResolveBlock,
               rejecter reject: @escaping RCTPromiseRejectBlock) {
 
-    guard let rootVC = UIApplication.shared.windows.first?.rootViewController else {
+    DispatchQueue.main.async {
+
+      // Localizar el root view controller
+      guard
+        let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+        let rootVC = scene.windows.first?.rootViewController
+      else {
         reject("NO_ROOT", "No root view controller", nil)
         return
-    }
+      }
 
-    let config = GIDConfiguration(
+      // Configuración correcta
+      let config = GIDConfiguration(
         clientID: "1050453988650-cq20qu63m02778k7ihkmghim6n0073og.apps.googleusercontent.com"
-    )
+      )
 
-    GIDSignIn.sharedInstance?().signIn
+      GIDSignIn.sharedInstance.configuration = config
+
+      // Sign-in moderno con presenting:
+      GIDSignIn.sharedInstance.signIn(withPresenting: rootVC) { result, error in
+
+        if let error = error {
+          reject("GOOGLE_ERROR", error.localizedDescription, error)
+          return
+        }
+
+        guard let user = result?.user else {
+          reject("NO_USER", "Google user is null", nil)
+          return
+        }
+
+        let idToken = user.idToken?.tokenString ?? ""
+        let accessToken = user.accessToken.tokenString
+
+        resolve([
+          "idToken": idToken,
+          "accessToken": accessToken,
+          "email": user.profile?.email ?? "",
+          "name": user.profile?.name ?? ""
+        ])
+      }
+    }
   }
 }
