92d14d20cb67847bdd8017859d6df705b3d7b8ee
lospueblosmasbonitosdeespana <lospueblosmasbonitosdeespana@gmail.com>
2025-11-16 17:47:02 +0100

    Añadido GoogleSignInModule.m (bridge nativo Google)

---
+++ ios/PueblosBonitosApp/GoogleSignInModule.m
@@ -0,0 +1,6 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(GoogleSignInModule, NSObject)
+RCT_EXTERN_METHOD(signIn:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+@end
