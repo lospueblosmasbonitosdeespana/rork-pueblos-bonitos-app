aaa957c526dfdb4ef15c3f5330ff9a5cf773523b
lospueblosmasbonitosdeespana <lospueblosmasbonitosdeespana@gmail.com>
2025-11-16 19:00:03 +0100

    Update GoogleSignInModule.m

--- ios/PueblosBonitosApp/GoogleSignInModule.m
+++ ios/PueblosBonitosApp/GoogleSignInModule.m
@@ -1,6 +1,10 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridge.h>
 
 @interface RCT_EXTERN_MODULE(GoogleSignInModule, NSObject)
+
 RCT_EXTERN_METHOD(signIn:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
+
 @end
